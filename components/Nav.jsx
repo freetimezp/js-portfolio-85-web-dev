71c1f61f548f991abb4dd5c4bb3e06adf859ff32
freetimezp <kraftukrnet@ukr.net>
2024-05-18 19:26:14 +0300

    02. js-portfolio-85-web-dev. Header, Nav component.

---
+++ components/Nav.jsx
@@ -0,0 +1,50 @@
+"use client";
+import React from 'react';
+import Link from 'next/link';
+
+import { usePathname } from 'next/navigation';
+
+const links = [
+    {
+        name: 'home',
+        path: '/'
+    },
+    {
+        name: 'services',
+        path: '/services'
+    },
+    {
+        name: 'resume',
+        path: '/resume'
+    },
+    {
+        name: 'work',
+        path: '/work'
+    },
+    {
+        name: 'contact',
+        path: '/contact'
+    }
+];
+
+const Nav = () => {
+    const pathname = usePathname();
+
+    return (
+        <nav className='flex gap-8'>
+            {links.map((link, index) => (
+                <Link
+                    key={index}
+                    href={link.path}
+                    className={`${link.path === pathname
+                        && "text-accent border-b-2 border-accent"} capitalize font-medium 
+                        hover:text-accent duration-300`}
+                >
+                    {link.name}
+                </Link>
+            ))}
+        </nav>
+    );
+}
+
+export default Nav;
